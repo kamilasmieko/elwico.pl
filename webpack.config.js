007eb060b8206f013efec8393d49a1ff34890e89
kamilasmieko <ksmieko@gmail.com>
2018-08-29 10:44:20 +0200

    start: building up all needed directory for the project

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: ['whatwg-fetch', "./src/js/app.jsx"],
+    output: {
+    filename: "./js/out.js"
+    },
+    devServer: {
+    inline: true,
+        contentBase: './',
+        port: 3001
+    },
+    watch: true,
+    module: {
+        loaders: [{
+            test: /\.jsx$/,  exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: { presets: [ 'es2015', 'stage-2', 'react'] }
+        }, {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }, {
+            test: /\.scss$/,
+            use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        }]
+    }
+}
