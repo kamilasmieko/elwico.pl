174acf91c31fc66e31270fcd6cac11129147e963
kamilasmieko <ksmieko@gmail.com>
2018-08-31 22:02:11 +0200

    banner: deciding on banner pics look

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -15,12 +15,55 @@ class Banner extends React.Component{
         let pics = ['src/img/banner_internet.png', 'src/img/tv_watching.png', 'src/img/banner_phone.png'];
 
         this.state = {
-            el: Array(3).fill({}).map((_, i) => ({id: i, isSelected: false}))
+            elements: Array(3).fill({}).map((_, i) => ({
+                id: i,
+                isSelected: false,
+                banner: pics[i]
+            })),
+            start: 0
         }
     }
     render(){
-        return <div>
-            
-                    </div>
+        return <div id='banner'
+                    className="slideshow-container"
+                    style={{height: '35rem'}}>
+
+                    {this.state.elements.map((el, i) => <div className="mySlides fade"
+                                                       key={i}
+                                                       style={el.isSelected?
+                                                           {display: 'block'} :
+                                                           {display: 'none'}}>
+                        <div className="carousel_pic"
+                             style={{backgroundImage: `url(${el.banner})`,
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundPosition: 'center',
+                                    backgroundSize: 'cover'}} />
+                    </div> )}
+
+                    <a className="prev" onClick={() => this.plusSlides(-1)}>&#10094;</a>
+                    <a className="next" onClick={() => this.plusSlides(1)}>&#10095;</a>
+                </div>
+    }
+    componentDidMount(){
+        this.showSlides(this.state.start);
+    }
+    showSlides(n){
+
+        n > this.state.elements.length-1 && this.setState({start: 0},
+            ()=> this.showSlides(0));
+        n < 0 && this.setState({start: this.state.elements.length},
+            () => this.showSlides(this.state.elements.length -1));
+
+        this.setState(prevState => ({
+            elements: prevState.elements.map(el => ({
+                id: el.id,
+                isSelected: el.id === n,
+                banner: el.banner
+            }))
+        }));
+    }
+    plusSlides = (val) =>{
+        this.setState(prevState => ({start: prevState.start + val}),
+            this.showSlides(this.state.start + val));
     }
 }
