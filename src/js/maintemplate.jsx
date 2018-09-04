1716f1c50b2b3bdf239100db7bf756ab5c48890b
kamilasmieko <ksmieko@gmail.com>
2018-09-04 13:21:24 +0200

    adding: transition on main menu hover

--- src/js/maintemplate.jsx
+++ src/js/maintemplate.jsx
@@ -8,7 +8,8 @@ import { Router,
 } from 'react-router';
 
 export class MainTemplate extends React.Component{
-    render(){
+    constructor(props){
+        super(props);
 
         const menuItems = [
             {to: '/tv', text: 'Telewizja'},
@@ -17,34 +18,47 @@ export class MainTemplate extends React.Component{
             {to: '/news', text: 'Aktualności'},
             {to: '/contact', text: 'Kontakt'}];
 
+
+        this.state = {
+            elements: menuItems.map((el, i) => ({
+                id: i,
+                isSelected: false,
+                to: el.to,
+                text: el.text
+            }))
+        }
+    }
+    render(){
+
         return <div>
                     <div className='container'>
-                        <Link to='/'><img id='logo' src="./src/img/elwico_logo.png" alt="logo" style={{width: '20rem'}}/></Link>
+
+                        <Link to='/'><img id='logo'
+                                          src="./src/img/elwico_logo.png"
+                                          alt="logo"
+                                          style={{width: '13rem'}}/></Link>
+
                         <ul className='main_menu'>
-                            {menuItems.map((el, i) => (<MenuItem key={i} to={el.to} text={el.text}/>))}
+                            {this.state.elements.map((el, i) => (
+                                <li key={i}>
+                                    <Link id={el.id}
+                                          to={el.to}
+                                          className={el.isSelected? 'active' : ''}
+                                          onClick={() => this.handleSelection(el.id)}>
+                                    {el.text}
+                                    </Link>
+                                </li>))}
                         </ul>
                     </div>
                     {this.props.children}
                 </div>
     }
-}
-
-class MenuItem extends React.Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-            isSelected: false
-        }
-    }
-    render(){
-
-        return <li><Link className={this.state.isSelected? 'active' : ''}
-                         to={this.props.to} onClick={this.handleSelection}>
-            {this.props.text}
-            </Link></li>
-    }
-    handleSelection = () => {
-        this.setState(prevState => ({isSelected: !prevState.isSelected}));
+    handleSelection = (id) => {
+        this.setState(prevState => ({elements: prevState.elements.map((el, i) => ({
+                id: el.id,
+                isSelected: el.id === id,
+                to: el.to,
+                text: el.text
+            }))}));
     }
 }
